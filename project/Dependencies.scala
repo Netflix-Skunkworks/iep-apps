1bcd644e2c324a020b000e19e9e0379ec893449d
Brian Harrington <brharrington@gmail.com>
2017-09-01 06:48:14 -0700

    log4j 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val iep        = "1.0.4"
     val guice      = "4.1.0"
     val jackson    = "2.8.9"
-    val log4j      = "2.8.1"
+    val log4j      = "2.9.0"
     val scala      = "2.12.2"
     val slf4j      = "1.7.25"
     val spectator  = "0.56.0"
