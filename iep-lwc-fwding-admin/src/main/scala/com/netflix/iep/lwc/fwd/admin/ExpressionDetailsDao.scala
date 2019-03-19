62416374936c9437ce8a94c83f9852c2d763fb6f
narayaruna <narayanan.arunachalam@gmail.com>
2019-03-18 23:32:48 -0700

    Remove unused import

--- iep-lwc-fwding-admin/src/main/scala/com/netflix/iep/lwc/fwd/admin/ExpressionDetailsDao.scala
+++ iep-lwc-fwding-admin/src/main/scala/com/netflix/iep/lwc/fwd/admin/ExpressionDetailsDao.scala
@@ -15,9 +15,7 @@
  */
 package com.netflix.iep.lwc.fwd.admin
 
-import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.document.Item
 import com.amazonaws.services.dynamodbv2.document.PrimaryKey
