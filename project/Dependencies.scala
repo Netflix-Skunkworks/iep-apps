169a4d08d51fa53f4fb0c1bb88766b5b9ebe57dc
Brian Harrington <brharrington@gmail.com>
2019-05-27 07:34:26 -0700

    iep 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val akkaHttpV  = "10.1.8"
     val atlas      = "1.6.0"
     val aws        = "1.11.556"
-    val iep        = "2.0.3"
+    val iep        = "2.0.4"
     val guice      = "4.1.0"
     val jackson    = "2.9.8"
     val log4j      = "2.11.2"
