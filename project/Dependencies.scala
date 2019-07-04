2322b135653ce5810a4b898c219b1fedd3396e6d
Brian Harrington <brharrington@gmail.com>
2019-07-04 13:04:41 -0700

    atlas 1.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val akka       = "2.5.23"
     val akkaHttpV  = "10.1.8"
-    val atlas      = "1.6.0"
+    val atlas      = "1.6.2"
     val aws        = "1.11.556"
     val iep        = "2.0.4"
     val guice      = "4.1.0"
