d21efd9d58b15e023f12ec86fd8ba7395c8ed454
Brian Harrington <brharrington@gmail.com>
2019-08-19 08:16:55 -0700

    akka-http 10.1.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val akka       = "2.5.24"
-    val akkaHttpV  = "10.1.8"
+    val akkaHttpV  = "10.1.9"
     val atlas      = "1.6.2"
     val aws        = "1.11.579"
     val iep        = "2.0.5"
