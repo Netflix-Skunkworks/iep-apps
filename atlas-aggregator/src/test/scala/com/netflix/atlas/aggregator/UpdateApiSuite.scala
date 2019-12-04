b3eb65e48e57b1374f896410baeff821d7e8764f
brharrington <brharrington@gmail.com>
2019-12-04 12:30:37 -0800

    validate datapoints coming into the aggregator (#182)
    The `/update` endpoint on the aggregator will now validate
    the datapoints and indicate any issues in the response. This
    can be used by thin clients to inform the user of problems.
    
    The rules are mostly the same as those used for the publish
    endpoint of the backend. Valid characters are not checked
    because they will get fixed on this path by the registry.

--- atlas-aggregator/src/test/scala/com/netflix/atlas/aggregator/UpdateApiSuite.scala
+++ atlas-aggregator/src/test/scala/com/netflix/atlas/aggregator/UpdateApiSuite.scala
@@ -15,7 +15,15 @@
  */
 package com.netflix.atlas.aggregator
 
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.StatusCode
+import akka.http.scaladsl.model.StatusCodes
 import com.fasterxml.jackson.core.JsonFactory
+import com.netflix.atlas.aggregator.UpdateApi.TagMap
+import com.netflix.atlas.akka.ByteStringInputStream
+import com.netflix.atlas.core.util.SmallHashMap
+import com.netflix.atlas.core.util.Strings
+import com.netflix.atlas.json.Json
 import com.netflix.spectator.api.Clock
 import com.netflix.spectator.api.Id
 import com.netflix.spectator.api.ManualClock
@@ -48,7 +56,7 @@ class UpdateApiSuite extends FunSuite {
         |  42.0
         |]
       """.stripMargin)
-    UpdateApi.processPayload(parser, service)
+    assert(UpdateApi.processPayload(parser, service).status === StatusCodes.OK)
     clock.setWallTime(62000)
     val id = Id.create("cpu").withTag(aggrTag)
     assert(service.lookup(id).counter(id).actualCount() === 42.0)
@@ -72,7 +80,7 @@ class UpdateApiSuite extends FunSuite {
         |  42.0
         |]
       """.stripMargin)
-    UpdateApi.processPayload(parser, service)
+    assert(UpdateApi.processPayload(parser, service).status === StatusCodes.OK)
     clock.setWallTime(62000)
     val id = Id
       .create("cpu")
@@ -99,7 +107,7 @@ class UpdateApiSuite extends FunSuite {
         |  42.0
         |]
       """.stripMargin)
-    UpdateApi.processPayload(parser, service)
+    assert(UpdateApi.processPayload(parser, service).status === StatusCodes.OK)
     clock.setWallTime(62000)
     val id = Id
       .create("cpu_user")
@@ -127,12 +135,104 @@ class UpdateApiSuite extends FunSuite {
         |  42.0
         |]
       """.stripMargin)
-    UpdateApi.processPayload(parser, service)
+    assert(UpdateApi.processPayload(parser, service).status === StatusCodes.OK)
     clock.setWallTime(62000)
     val id = Id
       .create("latency")
       .withTag(aggrTag)
       .withTag("percentile", "T0000")
     assert(service.lookup(id).counter(id).actualCount() === 42.0)
+  }
+
+  private def createPayload(ts: List[TagMap], op: Int, value: Double): String = {
+    val data = List.newBuilder[Any]
+    data += ts.map(_.size * 2).sum
+    ts.foreach { tags =>
+      tags.foreach { t =>
+        data += t._1
+        data += t._2
+      }
+    }
+    var offset = 0
+    ts.foreach { tags =>
+      data += tags.size
+      (0 until tags.size * 2).foreach { i =>
+        data += offset + i
+      }
+      data += op
+      data += value
+      offset += tags.size * 2
+    }
+    Json.encode(data)
+  }
+
+  private def validationTest(tags: TagMap, expectedStatus: StatusCode): FailureMessage = {
+    validationTest(List(tags), expectedStatus)
+  }
+
+  private def validationTest(ts: List[TagMap], expectedStatus: StatusCode): FailureMessage = {
+    val clock = new ManualClock()
+    val service = createAggrService(clock)
+    val parser = factory.createParser(createPayload(ts, 0, 1.0))
+    val response = UpdateApi.processPayload(parser, service)
+    assert(response.status === expectedStatus)
+    assert(response.entity.isStrict())
+    val strict = response.entity.asInstanceOf[HttpEntity.Strict]
+    Json.decode[FailureMessage](new ByteStringInputStream(strict.data))
+  }
+
+  test("validation: ok") {
+    val tags = SmallHashMap("name" -> "foo")
+    validationTest(tags, StatusCodes.OK)
+  }
+
+  test("validation: missing name") {
+    val tags = SmallHashMap("foo" -> "bar")
+    val msg = validationTest(tags, StatusCodes.BadRequest)
+    assert(msg.errorCount === 1)
+    assert(msg.message === List("missing 'name': Set(foo)"))
+  }
+
+  test("validation: too many user tags") {
+    val tags = Map("name" -> "foo") ++ (0 until 20)
+        .map(v => Strings.zeroPad(v, 5))
+        .map(v => v -> v)
+    val msg = validationTest(SmallHashMap(tags), StatusCodes.BadRequest)
+    assert(msg.errorCount === 1)
+    assert(msg.message === List("too many user tags: 21 > 20"))
+  }
+
+  test("validation: user tags, ignore restricted") {
+    val tags = Map("name" -> "foo", "nf.app" -> "www") ++ (0 until 19)
+        .map(v => Strings.zeroPad(v, 5))
+        .map(v => v -> v)
+    val msg = validationTest(SmallHashMap(tags), StatusCodes.OK)
+    assert(msg.errorCount === 0)
+  }
+
+  test("validation: tag rule") {
+    val tags = SmallHashMap("name" -> "test", "nf.foo" -> "bar")
+    val msg = validationTest(tags, StatusCodes.BadRequest)
+    assert(msg.errorCount === 1)
+    assert(msg.message === List("invalid key for reserved prefix 'nf.': nf.foo"))
+  }
+
+  test("validation: partial failure") {
+    val ts = List(
+      SmallHashMap("name" -> "test", "nf.foo" -> "bar"),
+      SmallHashMap("name" -> "test", "nf.app" -> "bar")
+    )
+    val msg = validationTest(ts, StatusCodes.Accepted)
+    assert(msg.errorCount === 1)
+    assert(msg.message === List("invalid key for reserved prefix 'nf.': nf.foo"))
+  }
+
+  test("validation: truncate if there are too many errors") {
+    val ts = (0 until 20).toList.map { i =>
+      SmallHashMap("name" -> i.toString, "nf.foo" -> "bar")
+    }
+    val msg = validationTest(ts, StatusCodes.BadRequest)
+    assert(msg.errorCount === 20)
+    assert(msg.message.size === 5)
   }
 }
