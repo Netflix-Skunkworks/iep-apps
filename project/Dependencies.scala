8e52781a729d962351e6f53cc65a0c8e1e586ad0
Brian Harrington <brharrington@gmail.com>
2020-05-05 14:55:53 -0500

    atlas 1.7.0-rc.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val akka       = "2.5.31"
     val akkaHttpV  = "10.1.11"
-    val atlas      = "1.7.0-rc.7"
+    val atlas      = "1.7.0-rc.8"
     val aws        = "1.11.764"
     val aws2       = "2.13.6"
     val iep        = "2.2.2"
