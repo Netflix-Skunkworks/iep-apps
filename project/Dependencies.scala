3555d8d489131559a50c56fc6171c6a1cafd5ce7
Brian Harrington <brharrington@gmail.com>
2020-12-14 10:59:19 -0600

    iep 2.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val atlas      = "1.7.0-SNAPSHOT"
     val aws        = "1.11.901"
     val aws2       = "2.15.44"
-    val iep        = "2.6.2"
+    val iep        = "2.6.4"
     val guice      = "4.1.0"
     val log4j      = "2.14.0"
     val scala      = "2.13.4"
