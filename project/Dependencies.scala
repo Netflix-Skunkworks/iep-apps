1e36d0a8ffb3dba8eebe9e5936f94ae884e05745
Brian Harrington <brharrington@gmail.com>
2021-04-29 07:57:46 -0500

    scala-logging 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   val log4jSlf4j         = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4j
   val scalaCompiler      = "org.scala-lang" % "scala-compiler" % scala
   val scalaLibrary       = "org.scala-lang" % "scala-library" % scala
-  val scalaLogging       = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+  val scalaLogging       = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.3"
   val scalaReflect       = "org.scala-lang" % "scala-reflect" % scala
   val scalatest          = "org.scalatest" %% "scalatest" % "3.2.5"
   val servoCore          = "com.netflix.servo" % "servo-core" % servo
