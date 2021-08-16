96353b26618331e081def7dc7228dc7904e2e3e6
Brian Harrington <brharrington@gmail.com>
2021-08-16 14:41:17 -0500

    akka-http 10.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val akka       = "2.6.15"
-    val akkaHttpV  = "10.2.4"
+    val akkaHttpV  = "10.2.6"
     val atlas      = "1.7.0-SNAPSHOT"
     val aws2       = "2.16.92"
     val iep        = "3.0.2"
