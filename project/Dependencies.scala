3d0a0335a1931964527796489c4c9360d19802ba
Brian Harrington <brharrington@gmail.com>
2023-12-06 14:40:38 -0600

    iep 5.0.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val atlas      = "1.8.0-SNAPSHOT"
     val aws2       = "2.21.38"
-    val iep        = "5.0.14"
+    val iep        = "5.0.15"
     val log4j      = "2.21.1"
     val pekko      = "1.0.2"
     val pekkoHttpV = "1.0.0"
