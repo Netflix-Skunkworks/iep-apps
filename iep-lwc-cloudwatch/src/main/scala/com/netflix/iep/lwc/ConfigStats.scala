4ba428812a08042b6e16fd91bec4d5778ae784f9
Brian Harrington <brharrington@gmail.com>
2024-02-07 19:50:19 -0600

    fix formatting

--- iep-lwc-cloudwatch/src/main/scala/com/netflix/iep/lwc/ConfigStats.scala
+++ iep-lwc-cloudwatch/src/main/scala/com/netflix/iep/lwc/ConfigStats.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2024 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.iep.lwc
 
 import com.netflix.iep.lwc.fwd.cw.ClusterConfig
@@ -68,7 +83,9 @@ class ConfigStats {
       snapshotConfigInfo(configInfo)
   }
 
-  private def snapshotConfigInfo(configInfo: ConcurrentHashMap[String, UriInfo]): List[Map[String, Any]] = {
+  private def snapshotConfigInfo(
+    configInfo: ConcurrentHashMap[String, UriInfo]
+  ): List[Map[String, Any]] = {
     configInfo
       .entrySet()
       .asScala
@@ -78,8 +95,8 @@ class ConfigStats {
         val uri = entry.getKey
         val info = entry.getValue
         ListMap(
-          "uri" -> uri,
-          "timestamp" -> Instant.ofEpochMilli(info.timestamp.get()).toString,
+          "uri"        -> uri,
+          "timestamp"  -> Instant.ofEpochMilli(info.timestamp.get()).toString,
           "datapoints" -> info.datapoints.get()
         )
       }
