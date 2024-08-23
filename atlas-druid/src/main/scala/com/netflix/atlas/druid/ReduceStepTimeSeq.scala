cd69cb4d5f1661bb98a6c28b294c818e6d93a5c8
Brian Harrington <brharrington@gmail.com>
2024-08-23 10:54:01 -0500

    druid: support data with mixed step sizes
    Update so that some data sources can be more granular than
    others and get blended together on the graphs. If a data
    source doesn't support the fine granularity, then it will
    be retrieved at its supported granularity and mapped to
    the requested step for the graph.
    
    This will not work for data sources that are consolidated
    historically. That would add quite a bit of complexity to
    processing the data, but may be desirable in the future
    to help mitigate cost.

---
+++ atlas-druid/src/main/scala/com/netflix/atlas/druid/ReduceStepTimeSeq.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014-2024 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.druid
+
+import com.netflix.atlas.core.model.DsType
+import com.netflix.atlas.core.model.TimeSeq
+
+class ReduceStepTimeSeq(ts: TimeSeq, val step: Long) extends TimeSeq {
+
+  require(
+    ts.step % step == 0,
+    "original step must be a multiple of reduced step"
+  )
+
+  def dsType: DsType = ts.dsType
+
+  def apply(timestamp: Long): Double = {
+    val t = timestamp / ts.step * ts.step
+    ts(t + ts.step)
+  }
+}
