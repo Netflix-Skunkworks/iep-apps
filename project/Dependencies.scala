11b9e4cadb298a743f725a4ef4de64f09121883e
Brian Harrington <brharrington@gmail.com>
2025-01-05 17:38:23 -0600

    aws-sdk-java-v2 2.29.45

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val atlas      = "1.8.0-SNAPSHOT"
-    val aws2       = "2.29.11"
+    val aws2       = "2.29.45"
     val iep        = "5.0.28"
     val log4j      = "2.24.1"
     val pekko      = "1.1.2"
