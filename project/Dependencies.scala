efe9853ba4c0ab1b001840f21a29c31b8a98769b
Brian Harrington <brharrington@gmail.com>
2025-09-10 13:23:48 -0500

    aws 2.33.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val atlas      = "1.8.0-rc.39"
-    val aws2       = "2.32.16"
+    val aws2       = "2.33.5"
     val iep        = "5.1.8"
     val log4j      = "2.25.1"
     val pekko      = "1.2.0"
