197dd514727b23645d2531202ad600cfb4766de1
Brian Harrington <brharrington@gmail.com>
2025-10-14 19:33:23 -0500

    scala 3.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val log4j      = "2.25.1"
     val pekko      = "1.2.0"
     val pekkoHttpV = "1.2.0"
-    val scala      = "3.7.1"
+    val scala      = "3.7.3"
     val servo      = "0.13.2"
     val slf4j      = "2.0.17"
     val spectator  = "1.9.0"
